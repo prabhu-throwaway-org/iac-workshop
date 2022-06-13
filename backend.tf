d4c1734ff043bba2c144ecd9f79bc709ce417b92
Niklas Sandström <mr.n.sandstrom@gmail.com>
2022-06-13 15:33:16 +0200

    add backend template

---
+++ backend.tf
@@ -0,0 +1,12 @@
+# This file is not used yet.
+# Uncomment when you have created a Storage Account, to save the state remotely
+
+# terraform {
+#   backend "azurerm" {
+#     subscription_id      = "replace-me"
+#     resource_group_name  = "replace-me"
+#     storage_account_name = "replace-me"
+#     container_name       = "tfbackend"
+#     key                  = "terraform-demo.tfstate"
+#   }
+# }
